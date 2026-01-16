839bcc1461e5341c5dbf81eccc1e679d5ffe0622
whitebelyash <whbexiumwork@gmail.com>
2026-01-16 22:45:16 +0400

    HACK: freedreno: force initial gmem offset only for 2+ slice GPUs
    On A810 this additional offset caused integer underflows because the remaining number was very small due to size of A810 GMEM cache (576K)

--- src/freedreno/common/fd6_gmem_cache.h
+++ src/freedreno/common/fd6_gmem_cache.h
@@ -43,7 +43,7 @@ __calc_gmem_cache_offsets(const struct fd_dev_info *info, unsigned offset,
    unsigned num_ccu = info->num_ccu;
 
    /* This seems not to be load bearing, but keeping it for now to match blob: */
-   if (info->chip >= 8)
+   if (info->chip >= 8 && info->num_slices > 1)
       offset -= 0x78000;
 
    config->vpc_bv_pos_buf_offset = offset - (num_ccu * config->vpc_bv_pos_buf_size);
